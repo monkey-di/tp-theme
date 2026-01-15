7c84a3b8ea0bd0582962cf9de98dae380e57b03f
ravencler@gmail.com <ravencler@gmail.com>
2026-01-15 11:36:17 +0700

    Fix

--- wp-content/themes/tebe-poveryat/template-parts/home/about-en.php
+++ wp-content/themes/tebe-poveryat/template-parts/home/about-en.php
@@ -4,7 +4,17 @@
  * Mobile First with Desktop version
  * Static content
  */
+
 ?>
+
+<style>
+    @media (min-width: 1280px) {
+        .about-en-section {
+            background-image: url("<?php echo get_theme_file_uri('assets/images/stars-bg.webp'); ?>");
+            background-position: center 8%;
+        }
+    }
+</style>
 <section class="about-en-section">
     <div class="about-en__container">
 
@@ -12,7 +22,9 @@
         <div class="about-en__left hidden lg:flex flex-col gap-10 pt-12 xl:pt-0 flex-[3]">
             <h2 class="about-en__title text-white">About Us</h2>
             <p class="about-en__description text-white">
-                We are a non-profit organization. We provide free psychological and legal support to people who experienced sexual violence in childhood. We are also actively engaged in prevention to protect children and make society safer.
+                We are a non-profit organization. We provide free psychological and legal support to people who
+                experienced sexual violence in childhood. We are also actively engaged in prevention to protect children
+                and make society safer.
             </p>
         </div>
 
@@ -22,7 +34,9 @@
             <div class="lg:hidden">
                 <h2 class="about-en__title text-white">About Us</h2>
                 <p class="about-en__description text-white mt-4">
-                    We are a non-profit organization. We provide free psychological and legal support to people who experienced sexual violence in childhood. We are also actively engaged in prevention to protect children and make society safer.
+                    We are a non-profit organization. We provide free psychological and legal support to people who
+                    experienced sexual violence in childhood. We are also actively engaged in prevention to protect
+                    children and make society safer.
                 </p>
             </div>
 
@@ -33,25 +47,25 @@
                 <!-- Statistics Cards -->
                 <div class="about-en__stats">
                     <!-- Card 1: Years -->
-                    <div class="about-en__stat-card bg-primary">
+                    <div class="about-en__stat-card about-en__stat-card--1 bg-primary">
                         <div class="about-en__stat-number">6</div>
-                        <p class="about-en__stat-text">years of dedicated work</p>
+                        <p class="about-en__stat-text">years of <br> dedicated work</p>
                     </div>
 
                     <!-- Card 2: Consultations -->
-                    <div class="about-en__stat-card bg-teal">
+                    <div class="about-en__stat-card about-en__stat-card--1 bg-teal">
                         <div class="about-en__stat-number">8 228</div>
-                        <p class="about-en__stat-text">consultations held with survivors and families</p>
+                        <p class="about-en__stat-text">consultations held with <br> survivors and families</p>
                     </div>
 
                     <!-- Card 3: People -->
-                    <div class="about-en__stat-card bg-sky">
+                    <div class="about-en__stat-card about-en__stat-card--2 bg-sky">
                         <div class="about-en__stat-number">2 091</div>
                         <p class="about-en__stat-text">people received psychological and legal support</p>
                     </div>
 
                     <!-- Card 4: Team -->
-                    <div class="about-en__stat-card bg-peach">
+                    <div class="about-en__stat-card about-en__stat-card--2 bg-peach">
                         <div class="about-en__stat-number">26</div>
                         <p class="about-en__stat-text">team members working together to make change possible</p>
                     </div>
--- wp-content/themes/tebe-poveryat/template-parts/home/details-en.php
+++ wp-content/themes/tebe-poveryat/template-parts/home/details-en.php
@@ -6,6 +6,16 @@
  */
 ?>
 <section class="details-en-section bg-surface relative z-20 pt-12 lg:pt-20 pb-12 lg:pb-32 overflow-hidden [border-radius:50%_50%_0_0_/_40px_40px_0_0] lg:[border-radius:50%_50%_0_0_/_80px_80px_0_0]">
+    <style>
+        @media (min-width: 1280px) {
+            .details-en__container {
+                background-image: url("<?php echo get_theme_file_uri('assets/images/bg-about.webp'); ?>");
+                background-position: right 75%;
+                background-repeat: no-repeat;
+            }
+        }
+
+    </style>
     <div class="details-en__container">
 
         <!-- Left Column (Title and Description) -->
@@ -15,6 +25,8 @@
                 AUTONOMOUS NON-PROFIT ORGANIZATION HELP FOR SURVIVORS OF SEXUAL VIOLENCE IN CHILDHOOD AND ADOLESCENCE, PREVENTION AND AVOIDANCE OF SEXUAL VIOLENCE AGAINST CHILDREN AND ADOLESCENTS "THEY WILL BELIEVE YOU"
             </p>
         </div>
+
+
 
         <!-- Right Column (Contact and Details) -->
         <div class="details-en__right">
