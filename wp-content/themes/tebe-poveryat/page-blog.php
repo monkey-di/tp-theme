a799acab757367d411ab1bce677039b1a4012d4f
WSPR-137 <wspr137@gmail.com>
2026-01-16 11:48:31 +0300

    #1139: шаблон страницы "Блог" (листинг)

--- wp-content/themes/tebe-poveryat/page-blog.php
+++ wp-content/themes/tebe-poveryat/page-blog.php
@@ -19,8 +19,8 @@ $pagehead_bg_color = get_field('headpage-background-color');  // ACF Цвет ф
 $pagehead_bg_image = get_field('headpage-background');  // ACF фоновое изображение
 $pagehead_pic = get_field('headpage-pic');  // ACF картинка
 ?>
-<main class="site-main">
-    <div class="page-head z-20 bg-surface relative [border-radius:0_0_50%_50%_/_0_0_40px_40px] lg:[border-radius:0_0_50%_50%_/_0_0_80px_80px]" style="background:<?=$pagehead_bg_color?>">
+<main class="site-main blog-page">
+    <div class="page-head z-20 bg-surface relative [border-radius:0_0_50%_50%_/_0_0_40px_40px] lg:[border-radius:0_0_50%_50%_/_0_0_80px_80px]"<?php if(<?php if($pagehead_bg_image != ''){?> != ''){?> style="background:<?=$pagehead_bg_color?>" <?php } ?>>
         <div class="page-head-wrapper container mx-auto"
             <?php if($pagehead_bg_image != ''){?>
                 style="background-image:url(<?=$pagehead_bg_image?>)"
