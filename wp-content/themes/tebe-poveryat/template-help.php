ffa94daf2a5f1edf4e691c260a41064673e98973
WSPR-137 <wspr137@gmail.com>
2026-01-16 10:05:19 +0300

    #201: шаблон страницы "Помощь"

--- wp-content/themes/tebe-poveryat/template-help.php
+++ wp-content/themes/tebe-poveryat/template-help.php
@@ -20,7 +20,7 @@ $pagehead_bg_image = get_field('headpage-background');  // ACF фоновое и
 $pagehead_pic = get_field('headpage-pic');  // ACF картинка
 ?>
 <main class="site-main">
-    <div class="page-head [border-radius:0_0_50%_50%_/_0_0_40px_40px] lg:[border-radius:0_0_50%_50%_/_0_0_80px_80px]" style="background:<?=$pagehead_bg_color?>">
+    <div class="page-head z-20 bg-surface relative [border-radius:0_0_50%_50%_/_0_0_40px_40px] lg:[border-radius:0_0_50%_50%_/_0_0_80px_80px]" style="background:<?=$pagehead_bg_color?>">
         <div class="page-head-wrapper container mx-auto" style="background:url(<?=$pagehead_bg_image?>)">
             <div class="page-head-col">
                 <h1 class="page-head-title">
