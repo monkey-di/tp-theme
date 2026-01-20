b143ddf6e1a41c8cbdedde836dbca233e465835a
WSPR-137 <wspr137@gmail.com>
2026-01-20 20:45:48 +0300

    #1139:блог

--- wp-content/themes/tebe-poveryat/template-parts/blocks/blog-intro/render.php
+++ wp-content/themes/tebe-poveryat/template-parts/blocks/blog-intro/render.php
@@ -1,23 +1,18 @@
 <?php
 // Простейший пример
-$title = get_field('text');
-$text = get_field('image');
-
-// ВРЕМЕННЫЙ КОД ДЛЯ ОТЛАДКИ - УДАЛИТЬ ПОСЛЕ
-echo '<pre style="background:#f1f1f1;padding:10px;border:1px solid red;">';
-echo 'ДАННЫЕ ДЛЯ ОТЛАДКИ (ID поста: ' . get_the_ID() . '):<br>';
-echo 'Заголовок: ' . htmlspecialchars($title) . '<br>';
-echo 'Текст: ' . htmlspecialchars($text);
-echo '</pre>';
-// КОНЕЦ ВРЕМЕННОГО КОДА
-?>
+$text = get_field('text');
+$image = get_field('image');
 
 <div class="blog-intro-block">
-    <?php if($title): ?>
-        <h2><?php echo esc_html($title); ?></h2>
-    <?php endif; ?>
+    <?php if($text){?>
+        <div class="blog-intro-block-text">
+        <?php echo wp_kses_post($text); ?>
+        </div>
+    <?php } ?>
 
-    <?php if($text): ?>
-        <div class="intro-text"><?php echo wp_kses_post($text); ?></div>
-    <?php endif; ?>
+    <?php if($image){ ?>
+        <div class="blog-intro-block-image">
+        <?php echo wp_kses_post($image); ?>
+        </div>
+    <?php } ?>
 </div>
