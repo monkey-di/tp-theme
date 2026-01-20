917e311ab7df68bd44fd2cc9f8473e0ae8895676
WSPR-137 <wspr137@gmail.com>
2026-01-20 07:03:23 +0300

    #1139:блог

--- wp-content/themes/tebe-poveryat/inc/post-types/register-post-types.php
+++ wp-content/themes/tebe-poveryat/inc/post-types/register-post-types.php
@@ -276,14 +276,14 @@ function tp_register_blog() {
 
     $args = array(
         'labels'              => $labels,
-        'public'              => true,
-        'publicly_queryable'  => true,
+        'public'              => false,
+        'publicly_queryable'  => false,
         'show_ui'             => true,
         'show_in_menu'        => true,
         'query_var'           => true,
-        'rewrite'             => array( 'slug' => 'blog' ),
+        'rewrite'             => false,
         'capability_type'     => 'post',
-        'has_archive'         => true,
+        'has_archive'         => false,
         'hierarchical'        => false,
         'menu_position'       => 86,
         'menu_icon'           => 'dashicons-edit-page',
