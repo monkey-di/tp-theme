9bf67e335806cbff94b0f099b09b578e14cd2617
WSPR-137 <wspr137@gmail.com>
2026-01-21 02:18:33 +0300

    #1139:блог

--- wp-content/themes/tebe-poveryat/template-parts/blocks/full-video/render.php
+++ wp-content/themes/tebe-poveryat/template-parts/blocks/full-video/render.php
@@ -1,3 +1,15 @@
 <?php
-
+/**
+ * Шаблон блока "Блок видео на всю ширину"
+ */
+$url = '';
+$image = '';
+if(function_exists('get_field')) {
+    $url = get_field('url');
+    $image = get_field('image');
+}
 ?>
+<div class="fullvideo-block">
+    <?php echo $url; ?>
+    <?php echo $image; ?>
+</div>
