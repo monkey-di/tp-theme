277085dc23a67c40cfeac250dc6adeac57ba087d
WSPR-137 <wspr137@gmail.com>
2026-01-20 23:14:14 +0300

    #1139:блог

--- wp-content/themes/tebe-poveryat/template-parts/blocks/slider-text/render.php
+++ wp-content/themes/tebe-poveryat/template-parts/blocks/slider-text/render.php
@@ -3,12 +3,22 @@
  * Шаблон блока "Блок слайдер + текст"
  * Работает в редакторе (админке) и на фронтенде
  */
+$slider = '';
 $text = '';
-$image = '';
 if(function_exists('get_field')) {
-    $text = get_field('text');
-    $image = get_field('image');
+    $slider = get_field('slider');
+    $text = get_field('slider-text');
 }
 
-$has_data = !empty($text) || !empty($image);
-?><?php
+$has_data = !empty($slider) || !empty($text);
+
+if ($has_data) { ?>
+    <div class="block-slider-text container">
+        <div class="block-slider-text_slider">
+            Слайдер
+        </div>
+        <div class="block-slider-text_text">
+            Текст
+        </div>
+    </div>
+<?php }
