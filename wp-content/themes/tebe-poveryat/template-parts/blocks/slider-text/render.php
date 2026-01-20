8a193cf3311645ff7a4b637a15179b33e0768059
WSPR-137 <wspr137@gmail.com>
2026-01-20 23:07:52 +0300

    #1139:блог

--- wp-content/themes/tebe-poveryat/template-parts/blocks/slider-text/render.php
+++ wp-content/themes/tebe-poveryat/template-parts/blocks/slider-text/render.php
@@ -1,1 +1,14 @@
 <?php
+/**
+ * Шаблон блока "Блок слайдер + текст"
+ * Работает в редакторе (админке) и на фронтенде
+ */
+$text = '';
+$image = '';
+if(function_exists('get_field')) {
+    $text = get_field('text');
+    $image = get_field('image');
+}
+
+$has_data = !empty($text) || !empty($image);
+?><?php
