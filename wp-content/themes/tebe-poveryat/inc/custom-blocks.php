0f6e077d8b7e2057323872fcb60331c42e33aade
WSPR-137 <wspr137@gmail.com>
2026-01-20 19:58:49 +0300

    #1139:блог

--- wp-content/themes/tebe-poveryat/inc/custom-blocks.php
+++ wp-content/themes/tebe-poveryat/inc/custom-blocks.php
@@ -1,5 +1,5 @@
 <?php
 add_action('init', 'register_custom_blocks');
 function register_custom_blocks() {
-    register_block_type( __DIR__ . '/template-parts/blocks/blog-intro' );
+    register_block_type('/wp-content/themes/tebt-poveryat/template-parts/blocks/blog-intro' );
 }
