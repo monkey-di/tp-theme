497966b47c3c4879923de1b83d12b6e3d1c7973b
WSPR-137 <wspr137@gmail.com>
2026-01-20 23:41:51 +0300

    #1139:блог

--- wp-content/themes/tebe-poveryat/single-blog_item.php
+++ wp-content/themes/tebe-poveryat/single-blog_item.php
@@ -7,6 +7,7 @@ get_header();
     <main class="site-main blog-single">
         <div class="container mx-auto">
             <?php echo apply_filters( 'the_content', '<!-- wp:acf/blog-intro /-->' ); ?>
+            <?php echo apply_filters( 'the_content', '<!-- wp:acf/slider-text /-->' ); ?>
         </div>
     </main>
 <?php
