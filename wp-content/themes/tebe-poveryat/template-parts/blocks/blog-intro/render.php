d089facae94c3c5a5a6464317f7bce3f0babea28
WSPR-137 <wspr137@gmail.com>
2026-01-20 21:02:38 +0300

    #1139:блог

--- wp-content/themes/tebe-poveryat/template-parts/blocks/blog-intro/render.php
+++ wp-content/themes/tebe-poveryat/template-parts/blocks/blog-intro/render.php
@@ -5,13 +5,13 @@ $image = get_field('image');
 <div class="blog-intro-block">
     <?php if($text){?>
         <div class="blog-intro-block-text">
-        <?php echo wp_kses_post($text); ?>
+            <?php echo wp_kses_post($text); ?>
         </div>
     <?php } ?>
 
     <?php if($image){ ?>
         <div class="blog-intro-block-image">
-         <img src="<?php echo $image; ?>">
+            <img src="<?php echo $image; ?>">
         </div>
     <?php } ?>
 </div>
