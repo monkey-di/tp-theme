933a2e2a27f24182dc5d7f0eb6690fdcc4803a83
WSPR-137 <wspr137@gmail.com>
2026-01-20 05:17:25 +0300

    #1139:блог

--- wp-content/themes/tebe-poveryat/inc/blog-ajax.php
+++ wp-content/themes/tebe-poveryat/inc/blog-ajax.php
@@ -1,4 +1,28 @@
 <?php
+// Функция для подсчета просмотров
+function set_post_views($postID) {
+    $count_key = 'views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if ($count == '') {
+        $count = 0;
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+    } else {
+        $count++;
+        update_post_meta($postID, $count_key, $count);
+    }
+}
+
+// Отслеживание просмотров поста
+function track_post_views($post_id) {
+    if (!is_single()) return;
+    if (empty($post_id)) {
+        global $post;
+        $post_id = $post->ID;
+    }
+    set_post_views($post_id);
+}
+add_action('wp_head', 'track_post_views');
 
 // Шорткод для AJAX-подгрузки кастомных постов
 add_shortcode('ajax_custom_posts', function($atts) {
