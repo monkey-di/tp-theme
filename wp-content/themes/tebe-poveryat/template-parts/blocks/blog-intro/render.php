e844ec530ee51e629370e6ba233f929acc8f8541
WSPR-137 <wspr137@gmail.com>
2026-01-20 20:42:40 +0300

    #1139:блог

--- wp-content/themes/tebe-poveryat/template-parts/blocks/blog-intro/render.php
+++ wp-content/themes/tebe-poveryat/template-parts/blocks/blog-intro/render.php
@@ -1,7 +1,15 @@
 <?php
 // Простейший пример
-$title = get_field('заголовок');
-$text = get_field('текст');
+$title = get_field('text');
+$text = get_field('image');
+
+// ВРЕМЕННЫЙ КОД ДЛЯ ОТЛАДКИ - УДАЛИТЬ ПОСЛЕ
+echo '<pre style="background:#f1f1f1;padding:10px;border:1px solid red;">';
+echo 'ДАННЫЕ ДЛЯ ОТЛАДКИ (ID поста: ' . get_the_ID() . '):<br>';
+echo 'Заголовок: ' . htmlspecialchars($title) . '<br>';
+echo 'Текст: ' . htmlspecialchars($text);
+echo '</pre>';
+// КОНЕЦ ВРЕМЕННОГО КОДА
 ?>
 
 <div class="blog-intro-block">
