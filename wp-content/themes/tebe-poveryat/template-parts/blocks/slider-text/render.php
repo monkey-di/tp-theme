9af579094793129f4c1269135aba0fcf46f1e90e
WSPR-137 <wspr137@gmail.com>
2026-01-20 23:25:38 +0300

    #1139:блог

--- wp-content/themes/tebe-poveryat/template-parts/blocks/slider-text/render.php
+++ wp-content/themes/tebe-poveryat/template-parts/blocks/slider-text/render.php
@@ -15,10 +15,22 @@ $has_data = !empty($slider) || !empty($text);
 if ($has_data) { ?>
     <div class="block-slider-text container">
         <div class="block-slider-text_slider">
-            Слайдер
+            <?php
+            if( have_rows('slider') ){ ?>
+                <div class="block-slider-text_slider-inner">
+                    <?php while ( have_rows('slider') ) {
+                        the_row();?>
+                        <div class="block-slider-text_slider-inner-item">
+                            <img src="<?php the_sub_field('sub_field_name'); ?>">
+                        </div>
+                    <? } ?>
+                </div>
+            <?php } ?>
         </div>
         <div class="block-slider-text_text">
-            Текст
+            <?php if($text){?>
+                <?php echo wp_kses_post($text); ?>
+            <?php } ?>
         </div>
     </div>
 <?php }
