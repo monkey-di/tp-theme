b5b3b82a7b0ad2d5dfbab1dfe1dad3f8eeab3719
WSPR-137 <wspr137@gmail.com>
2026-01-20 06:45:21 +0300

    #1139:блог

--- wp-content/themes/tebe-poveryat/assets/js/ajax-posts.js
+++ wp-content/themes/tebe-poveryat/assets/js/ajax-posts.js
@@ -4,7 +4,6 @@ jQuery(document).ready(function($) {
         const $wrapper = $container.find('.bloglist-wrapper');
         const $btn = $container.find('.load-more-btn');
         const $sort = $container.find('.sort-select');
-        const $spinner = $btn.find('.spinner');
 
         // Установка начальных значений
         $sort.val($container.data('sort'));
@@ -12,12 +11,8 @@ jQuery(document).ready(function($) {
 
         // Событие сортировки
         $sort.on('change', function() {
-            const newSort = $(this).val();
-            $container.data('sort', newSort);
+            $container.data('sort', $(this).val());
             $container.data('paged', 1);
-
-            $btn.hide();
-            $wrapper.html('<div class="loading">Загрузка...</div>');
 
             $.ajax({
                 url: ajax_object.ajax_url,
@@ -25,26 +20,25 @@ jQuery(document).ready(function($) {
                 data: {
                     action: 'load_more_custom_posts',
                     nonce: ajax_object.nonce,
-                    paged: 0, // Сбрасываем на первую страницу
+                    paged: 0,
                     post_type: $container.data('post-type'),
-                    sort: newSort,
+                    sort: $(this).val(),
                     per_page: $container.data('perpage'),
                     taxonomy: $container.data('taxonomy'),
                     term: $container.data('term'),
                     meta_key: $container.data('meta-key'),
                     meta_value: $container.data('meta-value')
                 },
+                beforeSend: function() {
+                    $wrapper.html('<div class="loading">Загрузка...</div>');
+                    $btn.hide();
+                },
                 success: function(response) {
                     if (response.success) {
                         $wrapper.html(response.data.html);
                         $container.data('paged', 1);
                         updateCustomButtonState($container);
-                    } else {
-                        $wrapper.html('<p class="error">Ошибка загрузки</p>');
                     }
-                },
-                error: function() {
-                    $wrapper.html('<p class="error">Ошибка загрузки</p>');
                 }
             });
         });
@@ -52,12 +46,13 @@ jQuery(document).ready(function($) {
         // Загрузка постов
         $btn.on('click', function(e) {
             e.preventDefault();
-            if ($(this).hasClass('disabled') || $(this).hasClass('loading')) return;
+            if ($(this).hasClass('disabled')) return;
 
             const currentPage = parseInt($container.data('paged'));
+            const $spinner = $(this).find('.spinner');
 
             $spinner.show();
-            $btn.addClass('loading');
+            $(this).addClass('loading');
 
             $.ajax({
                 url: ajax_object.ajax_url,
@@ -79,12 +74,12 @@ jQuery(document).ready(function($) {
                     $btn.removeClass('loading');
 
                     if (response.success) {
-                        // Удаляем старые данные и сообщение "нет постов"
-                        $wrapper.find('.posts-data, .no-posts-found').remove();
+                        // Удаляем старые данные
+                        $wrapper.find('.posts-data').remove();
 
                         // Добавляем новые посты
-                        const $newContent = $(response.data.html);
-                        $wrapper.append($newContent);
+                        const $newPosts = $(response.data.html);
+                        $wrapper.append($newPosts.find('*:not(.posts-data)'));
 
                         // Обновляем состояние
                         $container.data('paged', response.data.paged);
@@ -94,7 +89,6 @@ jQuery(document).ready(function($) {
                 error: function() {
                     $spinner.hide();
                     $btn.removeClass('loading');
-                    alert('Произошла ошибка при загрузке');
                 }
             });
         });
@@ -106,15 +100,11 @@ jQuery(document).ready(function($) {
         const $data = $container.find('.posts-data');
 
         if ($data.length) {
-            try {
-                const data = JSON.parse($data.html());
-                if (data.has_more) {
-                    $btn.show().removeClass('disabled');
-                } else {
-                    $btn.hide().addClass('disabled');
-                }
-            } catch (e) {
-                $btn.hide();
+            const data = JSON.parse($data.html());
+            if (data.has_more) {
+                $btn.show().removeClass('disabled');
+            } else {
+                $btn.hide().addClass('disabled');
             }
         } else {
             $btn.hide();
