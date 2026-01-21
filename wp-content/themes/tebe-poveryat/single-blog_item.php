986f439d4f4e9eef4165f3e92a827244f4420a89
WSPR-137 <wspr137@gmail.com>
2026-01-21 03:58:36 +0300

    #1139:блог

--- wp-content/themes/tebe-poveryat/single-blog_item.php
+++ wp-content/themes/tebe-poveryat/single-blog_item.php
@@ -7,6 +7,9 @@ get_header();
     <main class="site-main blog-single">
         <div class="container mx-auto">
             <?php the_content();?>
+            <div class="back-blog-button">
+                <a href="/bloglist/">Вернуться к списку новостей</a>
+            </div>
         </div>
     </main>
 <?php get_footer();
