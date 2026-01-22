5ec8b4bce48e8c1d09a4ee59a74767e8a3cba5e6
WSPR-137 <wspr137@gmail.com>
2026-01-22 12:21:15 +0300

    #1033:Получить юридическую помощь

--- wp-content/themes/tebe-poveryat/template-parts/blocks/faq-spoilers/render.php
+++ wp-content/themes/tebe-poveryat/template-parts/blocks/faq-spoilers/render.php
@@ -4,13 +4,19 @@
  * Работает в редакторе (админке) и на фронтенде
  */
 ?>
-<?php
-if( have_rows('faq-content') ){
-    while ( have_rows('faq-content') ) {
-        the_row();
-        the_sub_field('faq-question');
-        the_sub_field('faq-answer');
+<div class="faq-spoiler">
+<div class="faq-spoiler-content">
+    <?php
+    if( have_rows('faq-content') ){
+        while ( have_rows('faq-content') ) {
+            the_row(); ?>
+            <details>
+                <summary><?php wp_kses_post(the_sub_field('faq-question'));?></summary>
+                <div class="faq-details-content"><?php wp_kses_post(the_sub_field('faq-answer'));?></div>
+            </details>
+       <?php }
     }
-}
-?>
+    ?>
+    </div>
+</div>
 <?php
