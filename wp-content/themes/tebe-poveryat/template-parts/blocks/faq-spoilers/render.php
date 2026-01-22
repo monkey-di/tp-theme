a9b1b9ebe549f716661ed77bb14682cdcb426a12
WSPR-137 <wspr137@gmail.com>
2026-01-22 06:39:47 +0300

    #1139:блог

--- wp-content/themes/tebe-poveryat/template-parts/blocks/faq-spoilers/render.php
+++ wp-content/themes/tebe-poveryat/template-parts/blocks/faq-spoilers/render.php
@@ -1,1 +1,14 @@
 <?php
+/**
+ * Шаблон блока "Интро блога"
+ * Работает в редакторе (админке) и на фронтенде
+ */
+$text = '';
+$image = '';
+if(function_exists('get_field')) {
+    $faq = get_field('faq-content');
+}
+print_r($faq);
+?>
+
+<?php
