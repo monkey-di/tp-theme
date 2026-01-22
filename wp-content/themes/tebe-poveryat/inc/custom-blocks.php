2f56c4f2ac6c7c6b84481211e008078d28d364dc
WSPR-137 <wspr137@gmail.com>
2026-01-22 09:07:24 +0300

    #1033:Получить юридическую помощь

--- wp-content/themes/tebe-poveryat/inc/custom-blocks.php
+++ wp-content/themes/tebe-poveryat/inc/custom-blocks.php
@@ -4,4 +4,5 @@ function register_custom_blocks() {
     register_block_type( get_template_directory() . '/template-parts/blocks/blog-intro' );
     register_block_type( get_template_directory() . '/template-parts/blocks/slider-text' );
     register_block_type( get_template_directory() . '/template-parts/blocks/full-video' );
+    register_block_type( get_template_directory() . '/template-parts/blocks/faq-spoilers' );
 }
