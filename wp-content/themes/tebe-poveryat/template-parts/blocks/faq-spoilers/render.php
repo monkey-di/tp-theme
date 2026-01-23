d801921ded5716f5de302ea3903172c3ada5c31b
WSPR-137 <wspr137@gmail.com>
2026-01-23 10:30:32 +0300

    #1033:Получить юридическую помощь + плагин бронирования

--- wp-content/themes/tebe-poveryat/template-parts/blocks/faq-spoilers/render.php
+++ wp-content/themes/tebe-poveryat/template-parts/blocks/faq-spoilers/render.php
@@ -4,10 +4,11 @@
  * Работает в редакторе (админке) и на фронтенде
  */
 $faq_bg_color = get_field('faq-bg-color');
-$faq_bg_img = get_field('faq-bg-img')
+$faq_bg_img = get_field('faq-bg-img');
 ?>
 <div class="faq-spoiler" style="background-color:<?php echo $faq_bg_color; ?>;background-image:<?php echo $faq_bg_img; ?>">
 <div class="faq-spoiler-content">
+    <h2>Частые вопросы <br>и ответы</h2>
     <?php
     if( have_rows('faq-content') ){
         while ( have_rows('faq-content') ) {
