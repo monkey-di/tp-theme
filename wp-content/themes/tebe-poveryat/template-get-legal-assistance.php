ec97247bef5dbc1afa621b99b497e04d56f0d01a
WSPR-137 <wspr137@gmail.com>
2026-01-26 12:00:49 +0300

    #1033:Получить юридическую помощь + плагин бронирования

--- wp-content/themes/tebe-poveryat/template-get-legal-assistance.php
+++ wp-content/themes/tebe-poveryat/template-get-legal-assistance.php
@@ -254,7 +254,7 @@ $pagehead_pic = get_field('headpage-pic');  // ACF картинка
         }
 
         // Функция для добавления инлайн стиля display:none для блока .slotsCalendarfieldname1_1
-      /*  function addInitialHideStyle() {
+        function addInitialHideStyle() {
             const targetBlock = document.querySelector('.slotsCalendarfieldname1_1');
             if (targetBlock && !targetBlock.hasAttribute('data-initial-hide')) {
                 targetBlock.setAttribute('data-initial-hide', 'true');
@@ -262,7 +262,7 @@ $pagehead_pic = get_field('headpage-pic');  // ACF картинка
                 console.log('Блоку .slotsCalendarfieldname1_1 добавлен display:none');
             }
             return !!targetBlock;
-        }*/
+        }
 
         // Функция для добавления элементов в .slotsCalendar
         function decorateSlotsCalendar() {
@@ -806,7 +806,6 @@ $pagehead_pic = get_field('headpage-pic');  // ACF картинка
         } else {
             initialCheck();
         }
-        console.log('222222222222222222');
     </script>
 <?php
 get_footer();
