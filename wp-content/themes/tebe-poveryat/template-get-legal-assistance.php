b1ca7f39f05d747b519721f9e71a2b9a56b32ccc
WSPR-137 <wspr137@gmail.com>
2026-01-27 02:59:54 +0300

    #1033:Получить юридическую помощь + плагин бронирования

--- wp-content/themes/tebe-poveryat/template-get-legal-assistance.php
+++ wp-content/themes/tebe-poveryat/template-get-legal-assistance.php
@@ -484,6 +484,7 @@ $pagehead_pic = get_field('headpage-pic');  // ACF картинка
         } else {
             initialCheck();
         }
+        console.log('Кнопка скопирована');
     </script>
 <?php
 get_footer();
