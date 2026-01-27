51ee8125a344560e517b2dbb1fbf32b527c57810
WSPR-137 <wspr137@gmail.com>
2026-01-27 06:58:17 +0300

    #1033:Получить юридическую помощь + плагин бронирования

--- wp-content/themes/tebe-poveryat/template-get-legal-assistance.php
+++ wp-content/themes/tebe-poveryat/template-get-legal-assistance.php
@@ -34,14 +34,18 @@ $pagehead_pic = get_field('headpage-pic');  // ACF картинка
                     <div class="page-head-description text-p">
                         <?=$pagehead_description?>
                     </div>
-                    <a class="head-button" href="#anketa">Заполнить анкету</a>
+                    <div class="page-head-btn">
+                        <a class="head-button" href="#anketa">Заполнить анкету</a>
+                    </div>
                 </div>
                 <div class="page-head-col">
                     <div class="page-head-picture">
                         <img src="<?=$pagehead_pic?>">
                     </div>
                 </div>
-                <a class="head-button" href="#">Обратиться за помощью</a>
+                <div class="page-head-btn">
+                    <a class="head-button" href="#">Обратиться за помощью</a>
+                </div>
             </div>
         </div>
         <?php
